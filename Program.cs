8a61a1db2295cbd7f61e358df3a4179ac02c564b
buidinhhuy <buidinhhuy900@gmail.com>
2024-11-21 11:17:15 +0700

    no message

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Manage_tour
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new booktour());
+            Application.Run(new Entrymain());
         }
     }
 }
