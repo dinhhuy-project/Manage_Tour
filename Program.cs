2a1e756f49ce493a5d694cfe37acacda6b26a9b5
Cuonghoclaptrinh <manhcuong2004ml@gmail.com>
2024-11-23 00:23:17 +0700

    ManageTour

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Manage_tour
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Entrymain());
+            Application.Run(new Tour());
         }
     }
 }
