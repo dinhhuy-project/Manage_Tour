9abfd484a3007ee22ea33ea7d870e3c4995424b8
buidinhhuy <buidinhhuy900@gmail.com>
2024-12-02 13:20:53 +0700

    sửa lại cho thầy cường

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Manage_tour
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new customer());
+            Application.Run(new Entrymain());
         }
     }
 }
